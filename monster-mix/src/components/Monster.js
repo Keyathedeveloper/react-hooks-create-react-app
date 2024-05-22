625cc29fe531eada0abb6fbf07492630d5881d1a
Ta'Keya McFadden <takeyamcfadden@gmail.com>
2024-05-22 12:57:15 -0400

    created monster sketch working on adding items for customization

--- monster-mix/src/components/Monster.js
+++ monster-mix/src/components/Monster.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import p5 from 'p5';
 
-const Monster = ({ monster }) => {
-    return (
-        <div>
-            <img src ={monster.image} alt={monster.name} />
-        </div>
-    );
-};
+function Monster() {
+  const p5Ref = useRef();
+
+  const sketch = (p) => {
+    p.setup = () => {
+      p.createCanvas(400, 400);
+    };
+
+    p.draw = () => {
+      p.background(0);
+
+      //Draw monster's head
+      p.fill(255, 0, 0); // Red
+      p.ellipse(p.width / 2, p.height / 4, 100, 100);
+
+      //Draw eyes
+      p.fill(0); 
+
 
 export default Monster;
